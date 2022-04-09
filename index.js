5dcca3587d67d22067b3aed5583369ec81ec622e
Isabella <isabella.y.hu@gmail.com>
2022-04-09 20:24:38 +1000

    commit tree a699aef5680bda5e52a5f59c08e37a77fcd3c097

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue-42);
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return distanceFromHqInFeet(destination)-distanceFromHqInFeet(start);
+}
+
+function calculatesFarePrice(start,destination){
+    if(distanceTravelledInFeet(start,destination)<=400){
+        return 0;
+    } else if(distanceTravelledInFeet(start,destination)<=2000){
+        return (distanceTravelledInFeet(start,destination)-400)*0.02;
+    } else if(distanceTravelledInFeet(start,destination)<=2500){
+        return 25;
+    } else{
+        return 'cannot travel that far';
+    }
+
+}
